68e6d4832e01621180637bf93e71f3df6312da32
Marcogrdz <146217032+Marcogrdz@users.noreply.github.com>
2023-11-29 06:13:48 +0000

    Photo Create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -21,7 +21,18 @@ class PhotosController <ApplicationController
     the_photo= matching_photo.at(0)
 
     the_photo.destroy
-    
-    render ({ :template => "photo_templates/baii"})
+
+    redirect_to ("/photos")
     end
+
+  def create
+    new_photo = Photo.new
+    new_photo.image = params.fetch("query_image")
+    new_photo.caption = params.fetch("query_caption")
+    new_photo.owner_id = params.fetch("query_owner_id")
+    
+    new_photo.save
+      
+    redirect_to ("/photos/"+ new_photo.id.to_s)
+  end
 end
